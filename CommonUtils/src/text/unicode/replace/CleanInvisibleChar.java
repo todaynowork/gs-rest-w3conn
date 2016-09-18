eb7f103ac27ae59ade88df727fd60165a63c7c90
ZHI GUO DU <duzhiguo@cn.ibm.com>
2016-09-18 18:30:21 +0800

    add

--- CommonUtils/src/text/unicode/replace/CleanInvisibleChar.java
+++ CommonUtils/src/text/unicode/replace/CleanInvisibleChar.java
@@ -1,20 +1,27 @@
 package text.unicode.replace;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import common.Utils;
 
 public class CleanInvisibleChar {
 
+	private static final Logger logger = LoggerFactory.getLogger(CleanInvisibleChar.class); 
 	public static void main(String[] args){
-		String filePath = "/Users/zhiguodu/git/mtss_mobile/mtss_cli4_cn_ios/www/index.html";
+		String filePath = "/Users/zhiguodu/temp/index.html";
 		String my_string = Utils.readFile(filePath);
+		int originalIndex = my_string.indexOf("\u200B");
 		
 		String newString = my_string.replaceAll("\u200B", "");
+		newString = newString.replaceAll("\u00A0", "");
+		//newString = newString.replaceAll("[\\p{Zs}\\s]+", "");
 		//replaceAll("[\\p{C}\\p{Z}]", "")
-		System.out.print(newString);
+	
+		logger.debug(newString);
 		
+		
+		logger.debug(String.format("first index = %d", originalIndex));
+		logger.debug(String.format("first index = %d", newString.indexOf("\u200B")));
 	}
 }
